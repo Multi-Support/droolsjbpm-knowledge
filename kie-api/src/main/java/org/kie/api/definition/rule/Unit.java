3941dfb3b2792f65f031629d4827ef2bbb9d437c
testusr <marianbuenosayres@gmail.com>
2017-02-21 16:53:55 -0300

    # This is a combination of 3 commits. # The first commit's message is: JBPM-5710 KIE Persistence Cleanup
    # This is the 2nd commit message:
    
    [DROOLS-1446] ruleunit implementation (#210)
    
    # This is the 3rd commit message:
    
    Remove extra empty lines between 'package' and 'import'

--- kie-api/src/main/java/org/kie/api/definition/rule/Unit.java
+++ kie-api/src/main/java/org/kie/api/definition/rule/Unit.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2005 Red Hat, Inc. and/or its affiliates.
+ * Copyright 2017 Red Hat, Inc. and/or its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *       http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,12 +14,17 @@
  * limitations under the License.
  */
 
-package org.kie.api.event.kiebase;
+package org.kie.api.definition.rule;
 
-import org.kie.api.definition.KiePackage;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
-public interface AfterKiePackageRemovedEvent
-    extends
-    KieBaseEvent {
-    public KiePackage getKiePackage();
+import org.kie.api.runtime.rule.RuleUnit;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Unit {
+    Class<? extends RuleUnit> value();
 }
